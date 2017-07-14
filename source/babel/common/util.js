061e9180921c7e0dad33185edb7f9ff834090133
Maulik Anand <mbanandetoro6@gmail.com>
2017-07-14 20:42:47 +0530

    moved and renamed core to util

---
+++ source/babel/common/util.js
@@ -0,0 +1,18 @@
+// var jQuery = require('jquery') // require jquery for using http module
+
+function webRequest (url) { // web request to get data from urls
+  return new Promise((resolve, reject) => { // wrap into es6 promise
+    jQuery.get(url).then(resolve).catch(reject)
+  })
+}
+
+function jsonWebRequest (url) { // make web request and parse json into js object
+  return new Promise((resolve, reject) => { // wrap into es6 promise
+    jQuery.getJSON(url).then(resolve).catch(reject)
+  })
+}
+
+module.exports = { // export public apis
+  webRequest: webRequest,
+  jsonWebRequest: jsonWebRequest
+}
