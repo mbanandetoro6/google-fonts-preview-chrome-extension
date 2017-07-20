9913282ec1e01ef66eba53248239dcc7f106c2a7
Maulik Anand <mbanandetoro6@gmail.com>
2017-07-20 23:19:46 +0530

    replaced jquery ajax with axios

--- source/babel/common/util.js
+++ source/babel/common/util.js
@@ -1,18 +1,14 @@
-var jQuery = require('jquery') // require jquery for using http module
+var Axios = require('axios') // require jquery for using http module
 
 function webRequest (url) { // web request to get data from urls
   return new Promise((resolve, reject) => { // wrap into es6 promise
-    jQuery.get(url).then(resolve).catch(reject)
-  })
-}
-
-function jsonWebRequest (url) { // make web request and parse json into js object
-  return new Promise((resolve, reject) => { // wrap into es6 promise
-    jQuery.getJSON(url).then(resolve).catch(reject)
+    Axios.get(url).then((response) => {
+      resolve(response.data)
+    }).catch(reject)
   })
 }
 
 module.exports = { // export public apis
   webRequest: webRequest,
-  jsonWebRequest: jsonWebRequest
+  jsonWebRequest: webRequest
 }
