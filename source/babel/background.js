4623ba3b2145c66d6465121bcf9fe67e007e0489
Maulik Anand <mbanandetoro6@gmail.com>
2017-07-20 23:19:22 +0530

    cleanup & modify & commented code

--- source/babel/background.js
+++ source/babel/background.js
@@ -1,68 +1,64 @@
-/// <reference path="./../../exclude/chrome-dev-intelligence.d.ts"/>
 /* global chrome */
-const FontsApi = require('./background/fonts_background.js')
+
+import * as FontsApi from './background/fonts_background.js'
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function (tab) {
-  console.log('Clicked on browser action')
+chrome.browserAction.onClicked.addListener(init)
 
+function init (tab) {
   // inject the script into page
   chrome.tabs.executeScript(null, {
     file: 'js/main.js'
   })
-})
+}
 
-// handle incoming messages (single time messages)
+// on fonts request
 chrome.runtime.onMessage.addListener((message, sender, response) => {
-  if (message.request && message.request === 'fonts') {
-    FontsApi.getFonts()
-      .then((fonts) => {
-        response({
-          status: 'success',
-          fonts: fonts
-        })
-      })
-      .catch(() => {
-        response({
-          status: 'error',
-          message: 'failed to get fonts from server'
-        })
-      })
-    return true
+  if (message.request && message.request === 'fonts') { // if fonts is requested
+    onFontRequest(response) // handle request
+    return true // return true to keep chrome message line open, when using async function
   }
 })
 
-// handle incoming messages on channel
+function onFontRequest (response) {
+  FontsApi.getFonts() // get fonts with available cache
+    .then((fonts) => { // return fonts on success
+      response({
+        status: 'success',
+        fonts: fonts
+      })
+    })
+    .catch(() => { // on error
+      response({
+        status: 'error',
+        message: 'failed to get fonts from server'
+      })
+    })
+}
+
+// on fonts preview request, will be triggered when page open this message channel
 chrome.runtime.onConnect.addListener((port) => {
-  if (port.name === 'fontPreview') {
-    port.onMessage.addListener((message, port) => {
-      console.log(message)
-      if (message.request && message.request === 'fontPreview' && message.fontsWithoutPreview) {
-        var onProgress = (msg) => {
-          port.postMessage(msg)
-          if (msg.progress.isCompleted) {
-            port.disconnect()
-            console.log('port disconnect')
-          }
-        }
-        getFontsPreview(message.fontsWithoutPreview, onProgress)
+  if (port.name === 'fontPreview') { // verify message channel
+    port.onMessage.addListener((message, port) => { // on message
+      if (message.request && message.request === 'fontPreview' && message.fontsWithoutPreview) { // verify ? has data
+        FontsApi.getFontsPreviewImages(message.fontsWithoutPreview, (font, isSuccess, currentProgress) => { // call the font preview generate api
+          onPreviewProgress(font, isSuccess, currentProgress, port) // on preview progress callback
+        })
       }
     })
   } else {
-    port.disconnect()
+    port.disconnect() // on invalid port, close the message channel
   }
 })
 
-function getFontsPreview (fontsWithoutPreview, onProgress) {
-  console.log('generating Preview')
-  console.table(fontsWithoutPreview)
-  var onPreviewProgress = (font, isSuccess, currentProgress) => {
-    var msg = {
-      status: isSuccess ? 'success' : 'error',
-      font: font,
-      progress: currentProgress
-    }
-    onProgress(msg)
+function onPreviewProgress (font, isSuccess, currentProgress, port) { // handle on progress callback
+  var msg = { // build a message to be sent to the page on progress
+    status: isSuccess ? 'success' : 'error',
+    font: font,
+    progress: currentProgress
   }
-  FontsApi.getFontsPreviewImages(fontsWithoutPreview, onPreviewProgress)
+  port.postMessage(msg) // send message / font status to the page
+  if (msg.progress.isCompleted) { // on all font preview render complete
+    port.disconnect() // disconnect the messaging channel
+  }
 }
--- source/babel/background/fonts_background.js
+++ source/babel/background/fonts_background.js
@@ -1,185 +1,173 @@
-// import modules
-const WebFontLoader = require('webfontloader')
-const jQuery = require('jquery')
-// local imports
-const util = require('./../common/util.js')
-const storage = require('./../common/storage.js')
+const WebFontLoader = require('webfontloader') // font loader to load fonts and get callback
+const util = require('./../common/util.js') // common functions mostly ajax
+const storage = require('./../common/storage.js') // store and retrieve font cache
 
-const fontUrlBase = 'https://fonts.googleapis.com/css?family='
+const fontUrlBase = 'https://fonts.googleapis.com/css?family=' // url used to build font url for google font family
 // const apiUrl = 'https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha&fields=items(category%2Cfamily%2ClastModified%2Csubsets%2Cvariants)&key=AIzaSyBg1SCUmPcujiFq9gerb9rrozsLfjBTO8E'
-// const apiUrl = 'http://cdn.localhost.com/temp/google-fonts.json' // temp for local testing
-const apiUrl = 'http://cdn.localhost.com/temp/fonts-limited.json' // temp for local testing
-var fonts = []
+// const apiUrl = 'http://cdn.localhost.com/temp/google-fonts.json' // temp for local testing // [TODO][REMOVE] this in production
+const apiUrl = 'http://cdn.localhost.com/temp/fonts-limited.json' // limited fonts for local testing purpose // [TODO][REMOVE] this in production
 
-var getFonts = () => {
+var fonts = [] // initialize with empty array, to be filled later by api
+
+export function getFonts () { // exported function to be used to get fonts
   return new Promise((resolve, reject) => {
-    if (fonts.length > 0) {
-      resolve(fonts)
-      console.log('resolved from variable')
+    if (fonts.length > 0) { // if available in local fonts variable
+      resolve(fonts) // return with fonts
       return
     }
-    util.jsonWebRequest(apiUrl)
-      .then((response) => {
-        buildFonts(response.items)
-        mergeCache().then(resolve).catch(() => {
-          resolve(fonts)
+    // if not available in local variable
+    util.jsonWebRequest(apiUrl) // request the google fonts api
+      .then((response) => { // on success
+        buildFonts(response.items) // generate custom properties for each font
+        mergeCache().then(resolve).catch(() => { // if cache is available then merge with cache
+          resolve(fonts) // return fonts
         })
-        console.log('resolved from api')
       })
-    // .catch(reject)
+      .catch(reject) // on error
   })
 }
 
-var buildFonts = (rawFontsList) => {
-  var filteredFonts = rawFontsList.filter((font) => {
+function buildFonts (rawFontsList) { // generate custom properties for each fonts
+  var filteredFonts = rawFontsList.filter((font) => { // filter only english(latin) supported fonts
     return font.subsets.includes('latin')
   })
-  var newFonts = filteredFonts.map((font) => {
+  var newFonts = filteredFonts.map((font) => { // modify each font object in array
     var _font = font
-    _font.id = ('font_' + _font.family.replace(/\s+/g, '') + _font.lastModified.replace(/\D+/g, '')).toLowerCase()
-    var name = _font.family.replace(/\s+/g, '+')
-    _font.url = `${fontUrlBase}${name}:${_font.variants.join(',')}`
-    var previewText = encodeURIComponent(_font.family)
-    var previewVariant = _font.variants.includes('regular') ? '400' : _font.variants[0]
-    _font.previewUrl = `${fontUrlBase}${name}:${previewVariant}&text=${previewText}`
-    delete _font.subsets
-    delete _font.lastModified
+    _font.id = ('font_' + _font.family.replace(/\s+/g, '') + _font.lastModified.replace(/\D+/g, '')).toLowerCase() // generate unique id from font name and last modified date
+    var name = _font.family.replace(/\s+/g, '+') // generate name , replace space with plus sign, in existing font name
+    _font.url = `${fontUrlBase}${name}:${_font.variants.join(',')}` // generate font url, to be used when loading font in page
+    var previewText = encodeURIComponent(_font.family) // text for preview utl (font name is used)
+    var previewVariant = _font.variants.includes('regular') ? '400' : _font.variants[0] // get supported variant if regular is not supported
+    _font.previewUrl = `${fontUrlBase}${name}:${previewVariant}&text=${previewText}` // build preview url, text parameter in query string will cause google api to respond with limited characters and smallest possible file size
+    delete _font.subsets // not required afterwords, so delete
+    delete _font.lastModified // not required afterwords, so delete
     return _font
   })
-  fonts = newFonts
+  fonts = newFonts // save the generated fonts(with custom properties) to the local variable fonts
 }
-var mergeCache = () => {
+
+function mergeCache () { // this function will check for cache in storage, and merge with it if available
   return new Promise((resolve, reject) => {
-    storage.fonts.get().then((cachedFonts) => {
-      // console.log(cachedFonts)
-      var mergedFonts = fonts.map((font) => {
-        var _font = font
-        var cachedFont = cachedFonts.find((cFont) => cFont.id === _font.id)
-        if (cachedFont && cachedFont.base64Url) {
-          _font.base64Url = cachedFont.base64Url
-        }
-        return _font
+    storage.fonts.get() // try to get font cache from storage
+      .then((cachedFonts) => { // on success
+        var mergedFonts = fonts.map((font) => { // merge with fonts, for matched fonts
+          var _font = font
+          var cachedFont = cachedFonts.find((cFont) => cFont.id === _font.id) // find cache by id
+          if (cachedFont && cachedFont.base64Url) { // if cache is available
+            _font.base64Url = cachedFont.base64Url // merge with cache
+          }
+          return _font
+        })
+        fonts = mergedFonts // save fonts with cache to local variable
+        resolve(fonts) // return fonts with cache
+      }).catch(() => { // on error
+        reject(new Error('0 cached fonts found')) // reject with error
       })
-      fonts = mergedFonts
-      resolve(fonts)
-    }).catch(() => {
-      console.log('fonts not found')
-      reject(new Error('fonts not found'))
-    })
   })
-  // chrome.storage.local.get(null,function(data){console.log(data)})
 }
-var loadFontFamilyForPreview = (font) => {
+
+function loadFontFamilyForPreview (font) { // web font loader to load font family and provide on load callback event
   return new Promise((resolve, reject) => {
     WebFontLoader.load({
-      classes: false,
+      classes: false, // dont apply any event class to html tag
       custom: {
-        families: [font.family],
-        urls: [font.previewUrl]
+        families: [font.family], // font name
+        urls: [font.previewUrl] // font family css url
       },
       fontactive: function () {
-        resolve(font)
+        resolve(font) // on successful font load
       },
       fontinactive: function () {
-        reject(font)
+        reject(font) // on failed font load
       }
     })
   })
 }
 
-var getFontsPreview = (fontsToProcess, onProgressCallback) => {
+export function getFontsPreviewImages (fontsToProcess, onProgressCallback) { // generate a preview for provided fonts
+  // canvas settings, and scale variable to easily change values
   var scale = 2
   var settings = {
     fontSize: 18 * scale,
     height: 40 * scale,
     width: 330 * scale
   }
-  var canvas = document.createElement('canvas')
-  var successFonts = []
-  var errorFonts = []
+  var canvas = document.createElement('canvas') // canvas element to be used to render fonts preview , use one canvas element for all fonts, to improve performance
+  var successFonts = [] // gather all success fonts
+  var failedFonts = [] // gather all failed fonts
 
-  fontsToProcess.reduce((lastPromise, currentFont, index) => {
-    var onProgress = () => {
-      var isCompleted = successFonts.length + errorFonts.length === fontsToProcess.length
-      if (isCompleted) {
-        onPreviewRenderComplete(fontsToProcess, successFonts, errorFonts)
+  fontsToProcess.reduce((lastPromise, currentFont, index) => { // process fonts one by one using array reduce trick on promise based function
+    function onProgress () { // on progress callback
+      var isCompleted = successFonts.length + failedFonts.length === fontsToProcess.length // check if all fonts are processed
+      if (isCompleted) { // on complete callback
+        onPreviewRenderComplete(fontsToProcess, successFonts, failedFonts)
       }
-      return {
+      return { // return current progress data to be consumed by on progress callback
         isCompleted: isCompleted,
         successFonts: successFonts.length,
-        errorFonts: errorFonts.length,
+        errorFonts: failedFonts.length,
         totalFonts: fontsToProcess.length
       }
     }
-    var processFont = () => {
-      return generateFontPreview(currentFont, canvas, settings)
-        .then((_successFont) => {
-          successFonts.push(_successFont)
-          onProgressCallback(_successFont, true, onProgress())
-        }, (_errorFont) => {
-          errorFonts.push(_errorFont)
-          onProgressCallback(_errorFont, false, onProgress())
+
+    function processFont () { // process each font
+      return generateFontPreview(currentFont, canvas, settings) // process
+        .then((_successFont) => { // on success
+          successFonts.push(_successFont) // push to the success list
+          onProgressCallback(_successFont, true, onProgress()) // call progress callback function with succeeded font, and current progress data
+        }, (_errorFont) => { // on error
+          failedFonts.push(_errorFont) // push to failed fonts list
+          onProgressCallback(_errorFont, false, onProgress()) // call progress callback with error font, and current progress data
         })
     }
-    if (lastPromise === null) {
-      return processFont()
-    } else {
-      return lastPromise.then(processFont, processFont)
+    if (lastPromise === null) { // this will be empty in first iteration
+      return processFont() // so start with the current font, by using progress font function
+    } else { // from 2nd iteration this will return the promise from last function
+      return lastPromise.then(processFont, processFont) // so chain the process after function
     }
-  }, null)
+  }, null) // passing null , will be passed to the first iteration of reduce method
 }
 
-var onPreviewRenderComplete = (processedFonts, successFonts, errorFonts) => {
-  var updatedFonts = fonts.map((font) => {
+function onPreviewRenderComplete (processedFonts, successFonts, errorFonts) { // on font preview render complete callback
+  var updatedFonts = fonts.map((font) => { // save the rendered preview into local font variable
     var _font = font
-    var fontWithPreview = successFonts.find((sFont) => sFont.id === _font.id)
+    var fontWithPreview = successFonts.find((sFont) => sFont.id === _font.id) // match the font by id
     if (fontWithPreview && fontWithPreview.base64Url) {
       _font.base64Url = fontWithPreview.base64Url
     }
     return _font
   })
-  fonts = updatedFonts
-  storage.fonts.save(updatedFonts)
+  fonts = updatedFonts // save the updated fonts with preview to the local variable
+  storage.fonts.save(updatedFonts) // replace the fonts in cache
 }
 
-var generateFontPreview = (font, canvas, settings) => {
+function generateFontPreview (font, canvas, settings) { // this function will generate base64 image preview of font using canvas
   return new Promise((resolve, reject) => {
-    loadFontFamilyForPreview(font).then((loadedFont) => {
-      var context = canvas.getContext('2d')
-      canvas.height = settings.height
-      canvas.width = settings.width
-      console.log(font.family, 'rendered for preview')
+    loadFontFamilyForPreview(font) // load font family in background page
+      .then((loadedFont) => { // on load success
+        var context = canvas.getContext('2d') // get canvas content to draw on
+        canvas.height = settings.height // set height
+        canvas.width = settings.width // set width
+        console.log(font.family, 'rendered for preview')
 
-      context.clearRect(0, 0, settings.width, settings.height)
+        context.clearRect(0, 0, settings.width, settings.height) // remove any previous rendered data from canvas, as we are using same canvas for each preview
 
-      context.fillStyle = '#ffffff'
-      context.fillRect(0, 0, settings.width, settings.height)
+        context.fillStyle = '#ffffff' // set the background to white, note we are using jpeg output
+        context.fillRect(0, 0, settings.width, settings.height) // fill the canvas with white background
 
-      context.fillStyle = '#000000'
+        context.fillStyle = '#000000' // now make the fill style black, for font rendering
 
-      context.font = `${settings.fontSize}px '${loadedFont.family}'`
-      context.textAlign = 'left'
-      context.textBaseline = 'middle'
-      context.webkitImageSmoothingEnabled = false
-      context.mozImageSmoothingEnabled = false
-      context.imageSmoothingEnabled = false /// future
-      context.fillText(loadedFont.family, 0, settings.height / 2)
-      var dataUrl = canvas.toDataURL('image/jpeg', 1)
-      jQuery('head').find(`link[href="${loadedFont.previewUrl}"]`).remove()
-      font.base64Url = dataUrl
-      resolve(font)
-      // saveFontPreviewInCache(dataUrl).then(resolve, reject)
-    }).catch(reject)
+        context.font = `${settings.fontSize}px '${loadedFont.family}'` // provide font size and font family to canvas
+        context.textAlign = 'left' // horizontal text align from the point of starting
+        context.textBaseline = 'middle' // vertical alignment from the point of starting
+        // context.webkitImageSmoothingEnabled = false // enable this if found any font blurring issue on resize
+        // context.imageSmoothingEnabled = false
+        context.fillText(loadedFont.family, 0, settings.height / 2) // draw text on canvas
+        var dataUrl = canvas.toDataURL('image/jpeg', 1) // export to data url, we are using jpeg to pass quality value to 100%, png defaults to only 92%
+        document.querySelector(`head link[href="${loadedFont.previewUrl}"]`).remove() // remove the appended font link from the head
+        font.base64Url = dataUrl // save the base 64 utl in font properties
+        resolve(font) // return/resolve promise with generated font base 64 preview data
+      })
+      .catch(reject) // reject the promise on font loading error
   })
-}
-
-// var saveFontPreviewInCache = (font, dataUrl) => {
-//   return new Promise((resolve, reject) => {
-//     storage.set()
-//   })
-// }
-
-module.exports = {
-  getFonts: getFonts,
-  getFontsPreviewImages: getFontsPreview
 }
