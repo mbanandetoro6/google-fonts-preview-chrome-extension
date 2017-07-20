0f240ddab2da7a31a2110d707b0f7f8813d691b4
Maulik Anand <mbanandetoro6@gmail.com>
2017-07-20 23:45:49 +0530

    added comments

--- source/babel/common/util.js
+++ source/babel/common/util.js
@@ -2,13 +2,15 @@ var Axios = require('axios') // require jquery for using http module
 
 function webRequest (url) { // web request to get data from urls
   return new Promise((resolve, reject) => { // wrap into es6 promise
-    Axios.get(url).then((response) => {
-      resolve(response.data)
-    }).catch(reject)
+    Axios.get(url)
+      .then((response) => { // on success
+        resolve(response.data) // return with data
+      })
+      .catch(reject) // on error reject with error
   })
 }
 
 module.exports = { // export public apis
   webRequest: webRequest,
-  jsonWebRequest: webRequest
+  jsonWebRequest: webRequest // axios automatically parses the json data, so we are using the same function for both, but leaving the output api intact, incase have to change
 }
